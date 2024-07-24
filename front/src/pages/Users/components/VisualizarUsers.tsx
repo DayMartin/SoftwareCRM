b43aa20da43b5936b69db8771a5ab0be597a8023
DayMartin <negociosdinah@gmail.com>
2024-07-24 01:00:29 -0300

    fix: alterando grid de visualiar e editar dados do user 3

--- front/src/pages/Users/components/VisualizarUsers.tsx
+++ front/src/pages/Users/components/VisualizarUsers.tsx
@@ -1,7 +1,5 @@
-// src/components/VisualizarUsers.tsx
-
 import * as React from "react";
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Typography } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Typography, Grid } from "@mui/material";
 import { IListagemCliente } from "../../../shared/services/api/Users/UsersService";
 
 interface ClienteDialogProps {
@@ -39,93 +37,110 @@ const ClienteDialog: React.FC<ClienteDialogProps> = ({ open, onClose, client, on
             <DialogTitle>{isEditing ? "Editar Cliente" : "Detalhes do Cliente"}</DialogTitle>
             <DialogContent>
                 {editClient ? (
-                    <>
-                        <TextField
-                            required
-                            id="id"
-                            label="ID"
-                            name="id"
-                            value={editClient.id}
-                            fullWidth
-                            margin="normal"
-                            disabled
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            required
-                            id="nome"
-                            label="Nome"
-                            name="nome"
-                            value={editClient.nome}
-                            fullWidth
-                            margin="normal"
-                            disabled={!isEditing}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            required
-                            id="cpfcnpj"
-                            label="CPF/CNPJ"
-                            name="cpfcnpj"
-                            value={editClient.cpfcnpj}
-                            fullWidth
-                            margin="normal"
-                            disabled={!isEditing}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            required
-                            id="email"
-                            label="Email"
-                            name="email"
-                            value={editClient.email}
-                            fullWidth
-                            margin="normal"
-                            disabled={!isEditing}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            required
-                            id="telefone"
-                            label="Telefone"
-                            name="telefone"
-                            value={editClient.telefone}
-                            fullWidth
-                            margin="normal"
-                            disabled={!isEditing}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            id="status"
-                            label="Status"
-                            name="status"
-                            value={editClient.status}
-                            fullWidth
-                            margin="normal"
-                            disabled
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            id="endereco"
-                            label="Endereço"
-                            name="endereco"
-                            value={editClient.endereco}
-                            fullWidth
-                            margin="normal"
-                            disabled={!isEditing}
-                            onChange={handleChange}
-                        />
-                        <TextField
-                            id="data_criacao"
-                            label="Data criação"
-                            name="data_criacao"
-                            value={editClient.data_criacao}
-                            fullWidth
-                            margin="normal"
-                            disabled
-                            onChange={handleChange}
-                        />
-                    </>
+                    <Grid container spacing={2}>
+                        {/* Grid para os Inputs */}
+                        <Grid item xs={12} sm={4}>
+                            <TextField
+                                required
+                                id="id"
+                                label="ID"
+                                name="id"
+                                value={editClient.id}
+                                fullWidth
+                                margin="normal"
+                                disabled
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <TextField
+                                required
+                                id="nome"
+                                label="Nome"
+                                name="nome"
+                                value={editClient.nome}
+                                fullWidth
+                                margin="normal"
+                                disabled={!isEditing}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <TextField
+                                required
+                                id="cpfcnpj"
+                                label="CPF/CNPJ"
+                                name="cpfcnpj"
+                                value={editClient.cpfcnpj}
+                                fullWidth
+                                margin="normal"
+                                disabled={!isEditing}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <TextField
+                                required
+                                id="email"
+                                label="Email"
+                                name="email"
+                                value={editClient.email}
+                                fullWidth
+                                margin="normal"
+                                disabled={!isEditing}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <TextField
+                                required
+                                id="telefone"
+                                label="Telefone"
+                                name="telefone"
+                                value={editClient.telefone}
+                                fullWidth
+                                margin="normal"
+                                disabled={!isEditing}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <TextField
+                                id="status"
+                                label="Status"
+                                name="status"
+                                value={editClient.status}
+                                fullWidth
+                                margin="normal"
+                                disabled
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={8}>
+                            <TextField
+                                id="endereco"
+                                label="Endereço"
+                                name="endereco"
+                                value={editClient.endereco}
+                                fullWidth
+                                margin="normal"
+                                disabled={!isEditing}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <TextField
+                                id="data_criacao"
+                                label="Data criação"
+                                name="data_criacao"
+                                value={editClient.data_criacao}
+                                fullWidth
+                                margin="normal"
+                                disabled
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                    </Grid>
                 ) : (
                     <Typography variant="body1">Nenhum cliente selecionado.</Typography>
                 )}
