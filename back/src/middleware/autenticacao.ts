8c8431cdcb6b0246598e308e91fe09e6ce0e1ab1
DayMartin <negociosdinah@gmail.com>
2024-09-27 23:03:13 -0300

    fix: correção autenticacao rotas back

--- back/src/middleware/autenticacao.ts
+++ back/src/middleware/autenticacao.ts
@@ -1,27 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
-const JWT_SECRET = "seuSegredoSuperSecreto";
+const JWT_SECRET = "83JDINBD$#@%SSA";
 
 interface AuthenticatedRequest extends Request {
     user?: string | JwtPayload; 
 }
 
 // Middleware para autenticar o token JWT
-export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) {
         return res.status(401).json({ error: "Token não fornecido" });
     }
 
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ error: "Token inválido ou expirado" });
-        }
-
-        req.user = user;
-        next();
-    });
+	try {
+		jwt.verify(token, JWT_SECRET)
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			statusCode: 500,
+			message: "Token inválido"
+		})
+	}
 };
